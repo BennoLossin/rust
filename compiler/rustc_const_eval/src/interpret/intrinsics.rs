2dc603963533ef75cd4fc078600825780d660333
Benno Lossin <benno.lossin@proton.me>
2025-08-27 21:01:19 +0200

    const eval of field types

--- compiler/rustc_const_eval/src/interpret/intrinsics.rs
+++ compiler/rustc_const_eval/src/interpret/intrinsics.rs
@@ -164,7 +164,6 @@ impl<'tcx, M: Machine<'tcx>> InterpCx<'tcx, M> {
                     ty::Adt(adt, _) => {
                         ConstValue::from_target_usize(adt.variants().len() as u64, &tcx)
                     }
-                    ty::Field(..) => todo!("field_projections"),
                     ty::Alias(..) | ty::Param(_) | ty::Placeholder(_) | ty::Infer(_) => {
                         throw_inval!(TooGeneric)
                     }
@@ -190,6 +189,7 @@ impl<'tcx, M: Machine<'tcx>> InterpCx<'tcx, M> {
                     | ty::CoroutineWitness(..)
                     | ty::UnsafeBinder(_)
                     | ty::Never
+                    | ty::Field(..)
                     | ty::Tuple(_)
                     | ty::Error(_) => ConstValue::from_target_usize(0u64, &tcx),
                 };
@@ -681,7 +681,7 @@ impl<'tcx, M: Machine<'tcx>> InterpCx<'tcx, M> {
                                     }
                                 }
                             }
-                            _ => todo!("field_projections"),
+                            _ => bug!("unsupported type for `field_of!`"),
                         }
                         bug!("should have found a field with the name")
                     }
